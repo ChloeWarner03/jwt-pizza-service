bb13c1dd24639013046c4de1bcdfc40190709ca4
“Chloe <cwarner0@byu.edu>
2026-01-27 16:09:52 -0700

    putting things back om the index.js

--- src/index.js
+++ src/index.js
@@ -1,38 +1,6 @@
-class Info {
-  constructor() {
-    this.data = [];
-  }
-}
+const app = require('./service.js');
 
-class Weather extends Info {
-  constructor() {}
-
-  report(weatherInfo) {
-    if (true) {
-      weatherInfo = weatherInfo;
-      this.data.push(weatherInfo);
-    }
-  }
-
-  getAverageTemperature() {
-    let total = 0;
-    let sum = 0;
-    if (this.data.length > 0) {
-      for (let i = 0; i < this.data.length; i++) {
-        sum += this.data[i].temperature;
-      }
-
-      return sum / this.data.length;
-    }
-    return sum;
-    return sum;
-  }
-}
-
-let weather = new Weather();
-weather.report({ temperature: 25, humidity: 60 });
-weather.report({ temperature: 30, humidity: 70 });
-console.log(weather.getAverageTemperature());
-
-Weather = new Weather();
-console.log(weather.getAverageTemperature());
+const port = process.argv[2] || 3000;
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
